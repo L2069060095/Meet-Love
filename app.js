48579aa3cb44bf0591ab51a950d9ab3477df6d69
healer0519 <3156369743@qq.com>
2022-04-29 11:03:43 +0800

    项目初始化

---
+++ app.js
@@ -0,0 +1,105 @@
+const express = require("express");
+//当文件名是index时就不用再写
+const p1 = require("./router/p1");
+const p2 = require("./router/p2");
+const p3 = require("./router/p3");
+const p4 = require("./router/p4");
+const p5 = require("./router/p5");
+const p6 = require("./router/p6");
+const p7 = require("./router/p7");
+const p8 = require("./router/p8");
+const p9 = require("./router/p9");
+const p10 = require("./router/p10");
+const path = require("path");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+const expressSession = require("express-session");
+const app = express();
+
+//引入数据库
+mongoose.connect("mongodb://localhost/wedding");
+const db = mongoose.connection;
+db.on("error", console.error.bind(console, "connection error:"));
+db.once("open", function () {
+  console.log("已经连接数据库");
+});
+//配置静态资源目录
+app.use("/node_modules", express.static(path.join(__dirname, "node_modules")));
+app.use("/public", express.static(path.join(__dirname, "public")));
+
+//配置atr-template引擎
+app.engine("html", require("express-art-template"));
+
+//挂载body-parser 的中间件
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+//配置session
+// app.use(
+//   expressSession({
+//     name: "healer",
+//     secret: "secret",
+//     cookie: {
+//       maxAge: 1000 * 60 * 3,
+//     },
+//     resave: false,
+//     rolling: true,
+//     saveUninitialized: false,
+//   })
+// );
+// app.use((req, res, next) => {
+//   var path = req.path;
+//   //用户是否已登录
+//   var user = req.session.user;
+//   //登录了   登录和注册页面不能访问
+//   if (user) {
+//     if (path.startsWith("/login") || path.startsWith("/register")) {
+//       res.redirect("/index");
+//     } else {
+//       next();
+//     }
+//   }
+//   //没有登录   topic/new  和 setting开头的路径不能访问
+//   else {
+//     if (path.startsWith("/topic/new") || path.startsWith("/settings")) {
+//       res.redirect("/login");
+//     } else {
+//       next();
+//     }
+//   }
+// });
+//要写在body配置的后面
+//挂载路由
+app.use(p1);
+app.use(p2);
+app.use(p3);
+app.use(p4);
+app.use(p5);
+app.use(p6);
+app.use(p7);
+app.use(p8);
+app.use(p9);
+app.use(p10);
+
+
+//配置全局错误处理
+
+// 注意：404和500的错误处理中间件需要放在app.use(router)之后
+// 配置一个处理 404 的中间件
+//访问路径不存在的时候
+app.get("*", function (req, res) {
+  res.render("404.html", {
+    title: "您要找的页面不存在",
+  });
+});
+
+// 配置一个全局错误处理中间件
+app.use((err, req, res, next) => {
+  res.json({
+    code: 2002,
+    message: err.message,
+  });
+});
+app.listen(3000, () => {
+  console.log("3000端口已启用");
+});

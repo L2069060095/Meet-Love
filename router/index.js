872f43a2ab2e207b12215fb4f8fc14175fb45f3f
healer0519 <3156369743@qq.com>
2022-05-10 19:25:35 +0800

    黄蕾0510第二次修改

---
+++ router/index.js
@@ -0,0 +1,10 @@
+const express = require("express");
+const path = require("path");
+const mongoose = require("mongoose");
+const router = express.Router();
+
+router.get("/index", (req, res) => {
+    res.render("index.html");
+  });
+
+module.exports = router;
